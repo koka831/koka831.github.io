22614c13728ee15c510831da50c080573e40c8ce
koka <koka.code@gmail.com>
2021-01-28 16:34:34 +0900

    use scss

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,7 @@ import React from "react";
 import { AppProps } from "next/app";
 
 import "../styles/globals.css";
-import "prismjs/plugins/line-numbers/prism-line-numbers.css";
-import "../styles/prism-gruvbox.css";
+import "../styles/prism-gruvbox.scss";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return <Component {...pageProps} />;
