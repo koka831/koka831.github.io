20e56cfbe2fa85df817633730735134e4b10d3bd
koka <koka.code@gmail.com>
2021-01-31 18:21:41 +0900

    support katex

--- server/interpreter.ts
+++ server/interpreter.ts
@@ -1,8 +1,12 @@
 import remark from "remark";
 import headings from "remark-autolink-headings";
 import html from "remark-html";
+import math from "remark-math";
 import prism from "remark-prism";
 import slug from "remark-slug";
+import remark2rehype from "remark-rehype";
+import katex from "rehype-katex";
+import stringify from "rehype-stringify";
 
 const prismPlugins = [
   "autolinker",
@@ -13,9 +17,13 @@ const prismPlugins = [
 const markdownToHtml = async (markdown: string): Promise<string> => {
   const result = await remark()
     .use(html)
-    .use(prism, { plugins: prismPlugins })
+    .use(math)
+    .use(prism, { plugins: prismPlugins, transformInlineCode: true })
     .use(slug)
     .use(headings, { behavior: "wrap" })
+    .use(remark2rehype)
+    .use(katex)
+    .use(stringify)
     .process(markdown);
 
   return result.toString();
