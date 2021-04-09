365766948220072ccfaf7d8e211d8a19dbba43af
koka <koka.code@gmail.com>
2021-04-10 02:19:50 +0900

    add redirects from older

--- next.config.js
+++ next.config.js
@@ -1,3 +1,42 @@
 module.exports = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/2020/06/05/install-arch-linux(/?)",
+        destination: "/posts/2020-06-05-install-arch-linux/",
+        permanent: true
+      },
+      {
+        source: "/2019/11/18/abc132-e(/?)",
+        destination: "/posts/2019-11-18-abc132-e/",
+        permanent: true
+      },
+      {
+        source: "/2019/10/06/nuxt-sharing-component(/?)",
+        destination: "/posts/2019-10-06-nuxt-sharing-component/",
+        permanent: true
+      },
+      {
+        source: "/2019/08/12/abc110-d(/?)",
+        destination: "/posts/2019-08-12-abc110-d/",
+        permanent: true
+      },
+      {
+        source: "/2019/08/08/autolock-with-yubikey(/?)",
+        destination: "/posts/2019-08-08-autolock-with-yubikey/",
+        permanent: true
+      },
+      {
+        source: "/2019/07/27/linux-usergroup(/?)",
+        destination: "/posts/2019-07-27-linux-usergroup/",
+        permanent: true
+      },
+      {
+        source: "/2019/07/15/internal-vue(/?)",
+        destination: "/posts/2019-07-15-internal-vue/",
+        permanent: true
+      },
+    ];
+  }
 };
