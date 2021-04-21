b6be9c491f78730b90d674ee7e59a097619f45bd
koka <koka.code@gmail.com>
2021-04-21 23:55:30 +0900

    tweak base header meta

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -17,14 +17,16 @@ const MyApp: React.VFC<AppProps> = ({ Component, pageProps }: AppProps) => {
         <title>/var/log/koka</title>
         <meta key="title" name="title" content={title} />
         <meta key="description" name="description" content={description} />
-        <meta key="theme-color" name="theme-color" content="282828" />
+        <meta key="theme-color" name="theme-color" content="#ddd4bb" />
         <meta key="og:type" property="og:type" content="website" />
         <meta key="og:title" property="og:title" content={title} />
         <meta key="og:url" property="og:url" content={url} />
         <meta key="og:description" property="og:description" content={description} />
         <meta key="og:site_name" property="og:site_name" content={title} />
-        <meta name="twitter:card" content="summary_large_image" />
+        <meta key="og:image" property="og:image" content="https://koka831.github.io/img/icon.png" />
+        <meta name="twitter:card" content="summary" />
         <meta name="twitter:title" content={title} />
+        <meta name="twitter:site" content="@k_0ka" />
         <meta name="twitter:description" content={description} />
         <link rel="alternate" type="application/rss+xml" href="https://koka831.github.io/feed" title="/var/log/koka RSS2.0" />
       </Head>
