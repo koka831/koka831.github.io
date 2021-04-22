42c1959375dd6ed07eec84aa483e7fe44b691e66
koka <koka.code@gmail.com>
2021-04-22 15:05:57 +0900

    add CONST

---
+++ src/lib/const.ts
@@ -0,0 +1,5 @@
+export const HOMEPAGE_URL = "https://koka831.github.io";
+export const HOMEPAGE_TITLE = "/var/log/koka";
+export const HOMEPAGE_DESCRIPTION = "cat /var/log/koka > /dev/null";
+
+export const HOMEPAGE_THEME_COLOR = "#ddd4bb";
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,16 @@ import React from "react";
 import Head from "next/head";
 import { AppProps } from "next/app";
 
+import * as CONST from "../lib/const";
+
 import "../../node_modules/modern-normalize/modern-normalize.css";
 import "../styles/globals.scss";
 import "../styles/prism-gruvbox.scss";
 
 const MyApp: React.VFC<AppProps> = ({ Component, pageProps }: AppProps) => {
-  const url = "https://koka.dev";
-  const title = "/var/log/koka";
-  const description = "my log";
+  const url = CONST.HOMEPAGE_URL;
+  const title = CONST.HOMEPAGE_TITLE;
+  const description = CONST.HOMEPAGE_DESCRIPTION;
 
   return (
     <>
@@ -23,12 +25,12 @@ const MyApp: React.VFC<AppProps> = ({ Component, pageProps }: AppProps) => {
         <meta key="og:url" property="og:url" content={url} />
         <meta key="og:description" property="og:description" content={description} />
         <meta key="og:site_name" property="og:site_name" content={title} />
-        <meta key="og:image" property="og:image" content="https://koka831.github.io/img/icon.png" />
+        <meta key="og:image" property="og:image" content={`${url}/img/icon.png`} />
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:title" content={title} />
         <meta name="twitter:site" content="@k_0ka" />
         <meta name="twitter:description" content={description} />
-        <link rel="alternate" type="application/rss+xml" href="https://koka831.github.io/feed.xml" title="/var/log/koka RSS2.0" />
+        <link rel="alternate" type="application/rss+xml" href={`${url}/feed.xml`} title="/var/log/koka RSS2.0" />
       </Head>
       <Component {...pageProps} />
     </>
