c8e29efc45eb1d553f07e7d25aa995b047b66c5e
koka <koka.code@gmail.com>
2021-10-27 00:21:51 +0900

    update packages

--- src/lib/interpreter.ts
+++ src/lib/interpreter.ts
@@ -6,7 +6,7 @@ import headings from "remark-autolink-headings";
 import katex from "rehype-katex";
 import math from "remark-math";
 import prism from "remark-prism";
-import remark from "remark";
+import { remark } from "remark";
 import remark2rehype from "remark-rehype";
 import slug from "remark-slug";
 import stringify from "rehype-stringify";
@@ -26,7 +26,7 @@ export const markdownToHtml = async (markdown: string): Promise<string> => {
     .use(gfm)
     .use(math)
     .use(emoji)
-    .use(container)
+    // .use(container)
     .use(caption)
     .use(prism, { plugins: prismPlugins })
     .use(externalLink)
