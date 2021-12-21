5c42ce1de8899589e7ab7d3674c940e3c7b8fd8a
koka <koka.code@gmail.com>
2021-12-21 21:34:57 +0900

    chore: migrate remark-(slug|autolink-header) with rehype

--- src/lib/interpreter.ts
+++ src/lib/interpreter.ts
@@ -2,13 +2,13 @@ import emoji from "remark-emoji";
 import externalLink from "remark-external-links";
 import footnotes from "remark-footnotes";
 import gfm from "remark-gfm";
-import headings from "remark-autolink-headings";
+import headings from "rehype-autolink-headings";
 import katex from "rehype-katex";
 import math from "remark-math";
 import prism from "remark-prism";
-import { remark } from "remark";
+import {remark} from "remark";
 import remark2rehype from "remark-rehype";
-import slug from "remark-slug";
+import slug from "rehype-slug";
 import stringify from "rehype-stringify";
 
 import container from "remark-custom-container";
@@ -28,14 +28,14 @@ export const markdownToHtml = async (markdown: string): Promise<string> => {
     .use(emoji)
     .use(container)
     .use(caption)
-    .use(prism, { plugins: prismPlugins })
+    .use(prism, {plugins: prismPlugins})
     .use(externalLink)
-    .use(slug)
-    .use(headings, { behavior: "wrap" })
     .use(footnotes)
-    .use(remark2rehype, { allowDangerousHtml: true })
+    .use(remark2rehype, {allowDangerousHtml: true})
+    .use(slug)
+    .use(headings, {behavior: "wrap"})
     .use(katex)
-    .use(stringify, { allowDangerousHtml: true })
+    .use(stringify, {allowDangerousHtml: true})
     .process(markdown);
 
   return result.toString();
