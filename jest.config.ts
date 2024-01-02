4960cf29c1ab1819c42434bda01ec20b48e19a33
koka <koka.code@gmail.com>
2024-01-02 19:38:33 +0900

    bump
    fix jest
    
    remove annotation

--- jest.config.ts
+++ jest.config.ts
@@ -2,26 +2,26 @@
  * For a detailed explanation regarding each configuration property and type check, visit:
  * https://jestjs.io/docs/en/configuration.html
  */
-import { Config } from "@jest/types";
+import { Config } from "jest";
 
-const config: Config.InitialOptions = {
-  roots: ["<rootDir>/src"],
-  preset: "ts-jest/presets/js-with-ts-esm",
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
-  testEnvironment: "node",
-  transform: { "\\.ts$": "ts-jest" },
-  globals: {
-    "ts-jest": {
-      useESM: true,
-      tsconfig: {
-        jsx: "react",
-      },
+const config: Config = {
+    roots: ["<rootDir>/src"],
+    preset: "ts-jest/presets/js-with-ts-esm",
+    setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+    testEnvironment: "node",
+    transform: { "\\.ts$": "ts-jest" },
+    globals: {
+        "ts-jest": {
+            useESM: true,
+            tsconfig: {
+                jsx: "react",
+            },
+        },
     },
-  },
-  moduleNameMapper: {
-    "\\.(css|scss)$": "identity-obj-proxy",
-    "^(\\.{1,2}/.*)\\.js$": "$1",
-  },
+    moduleNameMapper: {
+        "\\.(css|scss)$": "identity-obj-proxy",
+        "^(\\.{1,2}/.*)\\.js$": "$1",
+    },
 };
 
 export default config;
