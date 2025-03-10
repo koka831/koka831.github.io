9e8601c402ce9807c7abcac0c46e55bc50cc6127
koka <koka.code@gmail.com>
2025-03-10 22:59:31 +0900

    bump remark/rehype
    upgrade
    
    rm
    
    rm

--- src/lib/interpreter.ts
+++ src/lib/interpreter.ts
@@ -1,25 +1,21 @@
 import emoji from "remark-emoji";
-import externalLink from "remark-external-links";
-import footnotes from "remark-footnotes";
+import externalLink from "rehype-external-links";
 import gfm from "remark-gfm";
 import headings from "rehype-autolink-headings";
 import katex from "rehype-katex";
 import math from "remark-math";
 import prism from "remark-prism";
 import { remark } from "remark";
+import remarkParse from "remark-parse";
 import remark2rehype from "remark-rehype";
+import rehypeRaw from "rehype-raw";
 import slug from "rehype-slug";
 import stringify from "rehype-stringify";
 
 import container from "remark-custom-container";
 import caption from "./remark-image-caption";
 
-const prismPlugins = [
-  "autolinker",
-  "command-line",
-  "diff-highlight",
-  "line-numbers",
-];
+const prismPlugins = ["autolinker", "command-line", "diff-highlight", "line-numbers"];
 
 export const markdownToHtml = async (markdown: string): Promise<string> => {
   const result = await remark()
@@ -29,10 +25,11 @@ export const markdownToHtml = async (markdown: string): Promise<string> => {
     .use(container)
     .use(caption)
     .use(prism, { plugins: prismPlugins })
-    .use(externalLink)
-    .use(footnotes)
+    .use(remarkParse)
     .use(remark2rehype, { allowDangerousHtml: true })
+    .use(rehypeRaw)
     .use(slug)
+    .use(externalLink)
     .use(headings, { behavior: "wrap" })
     .use(katex)
     .use(stringify, { allowDangerousHtml: true })
